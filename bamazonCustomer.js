9fa60ab8ad42760cf49162e4ccfac86e927fac8b
Will M <wpmcclung@gmail.com>
2019-01-08 13:36:48 -0600

    updated code to show semi working product

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,21 +1,26 @@
 const inquirer = require("inquirer");
-const table = require("table").table;
+// const table = require("table").table;
 const mysql = require("mysql");
+
+let currentDept;
+let userTotal;
 
 const connection = mysql.createConnection({
   host: "localhost",
 
   // Your port; if not 3306
   //changed to 3308 cause 3306 stopped working
-  port: 3308,
+  port: 3306,
 
   // Your username
   user: "root",
 
   // Your password
   password: "KhalilMack52",
-  database: "bamazonDB"
+  database: "bamazondb"
 });
+
+
 
 connection.connect(function(err) {
   if (err) throw err;
@@ -24,81 +29,153 @@ connection.connect(function(err) {
   // const productTable = new Table({
   //   head: ["ID", "brand", "shoename", "colorway", "price", "stock"]
   // });
-  start();
+  beginningPrompt();
 });
 
-const products = [
-  {
-    id: 1,
-    brand: "Adidas",
-    shoename: "yeezy 350's - sesame",
-    colorway: "sesame",
-    price: 249.0,
-    stock: 10
-  }
-];
 
-const start = () => {
-  // Running this application will first display all of the items available for sale.
-  //Include the ids, names, and prices of products for sale.
 
-  displayProducts();
-  //prompt app to user
-  getUserAction();
-  //The first should ask them the ID of the product they would like to buy.
-  // The second message should ask how many units of the product they would like to buy.
+function beginningPrompt() {
+  console.log('Welcome to BamaStockXYZ');
+  inquirer.prompt([
+    {
+    type: 'confirm',
+    name: 'confirm',
+    message: 'browse the rotation?',
+    default: true
+    }
+  ]).then(function(answers) {
+    if(answers.confirm) {
+      showAllShoes();
+    } else {
+      console.log('peace');
+      connection.end();
+    }
+  });
+}
+
+
+
+function promptUserInput() {
+  
   inquirer
     .prompt([
       {
-        name: "choice",
-        type: "input",
-        message: "Please enter ID of Sneaker you're interested in:"
+        type: 'input',
+			name: 'item',
+        message: "Please enter ID of Sneaker you're interested in:",
+        validate: function checkForIntegers(item) {
+          const reg = /^\d+$/;
+          return reg.test(item) || 'Please enter a numerical value for the ID.';
+        }
       },
       //if answer is correct prompt again
       {
-        name: "units",
         type: "input",
-        message: "How many:"
+        name: "itemQuantity",
+        message: "How many pairs would you like?",
+        validate: function checkForIntegers(itemQuantity) {
+          const reg = /^\d+$/;
+          return reg.test(itemQuantity) || 'Please enter a numerical value for your amount.';
+        }
       }
     ])
-    .then(answers => {
+    .then(function(answers) {
       //Validate that its an existing id using a filter
+        let sql = 'SELECT ?? FROM ?? WHERE = ?';
+        let values = ['*' , 'shoes' , 'id' , answers.item];
 
-      const isValid =
-        products.filter(product => product.id === answers.choice).length > 0;
+        sql = mysql.format(sql, values);
+		connection.query(sql, function(err, results) {
+			if (answers.itemQuantity <= results[0].stock) {
+				console.log('\nGreat choice!\n');
+				updateStock()
+				//update database
+				let amountPurchased = results[0].stock - answers.itemQuantity;
+				let updateStock = connection.query(
+					'UPDATE shoes SET ? WHERE ?',
+						[
+							{
+								stock: amountPurchased
+							},
+							{
+								id: results[0].id
+							}
+						],
+					function(err, results) {
+						console.log(`${results.affectedRows} product updated!`);
+						beginningPrompt();
+					}
+				);
 
-      //validate there is enough stock or output
-      //`Insufficient quantity!`
-
-      //Once the update goes through, show the customer the total cost of their purchase.
-      console.log(answers);
-      //*connect to the db and update the quanitiy
-
-      switch (
-        answers.choice
-        // case statements
-      ) {
+				// give user total 
+				userTotal = parseFloat((results[0].price * answers.itemQuantity).toFixed(2));
+				console.log(`Your total is $${userTotal}\n`);
+				
+				
+				
+				
+			} else {
+				console.log('\nNot enough in stock. \nPlease select less quanitity\n');
+				promptUserInput();
+			}
+		});		
+	});
+}
+      function showAllShoes() {
+        let sql = 'SELECT ?? FROM ??';
+        let values = ['*', 'shoes'];
+        sql = mysql.format(sql, values);
+        connection.query(sql, function(err, results, fields) {
+          if (err) throw err;
+          for (let i = 0; i < results.length; i++) {
+            console.log(` \nID: ${results[i].id}     Name: ${results[i].shoename}     Price: ${results[i].price} Colorway: ${results[i].colorway} Number in Stock: ${results[i].stock} \n-------------------------------------------------------------------------------------- \n`);
+          }
+          promptUserInput();
+        });
       }
-    });
-};
+      
+      function anotherChoice() {
+        inquirer.prompt([
+          {
+            type: 'confirm',
+            name: 'orderAgain',
+            message: 'Would you like to place another order?',
+            default: true
+          }
+        ]).then(function(answers) {
+          if (answers.orderAgain) {
+            anotherChoice();
+          } else {
+            console.log('Hope to see you again! Have a BAM!-tabulous day!');
+            connection.end();
+          }
+        });
+      }
+//       const isValid =
+//         products.filter(product => product.id === answers.choice).length > 0;
 
-const getUserAction = () => {};
-const displayProducts = () => {
-  //connect to db
-  // select products
-  //display each product (can use package for display)
+//       //validate there is enough stock or output
+//       //`Insufficient quantity!`
+
+//       //Once the update goes through, show the customer the total cost of their purchase.
+//       console.log(answers);
+//       //*connect to the db and update the quanitiy
+
+//       switch (
+//         answers.choice
+//         // case statements
+//       ) {
+//       }
+ 
+// };
+
+// const getUserAction = () => {};
+// const displayProducts = () => {
+//   //connect to db
+//   // select products
+//   //display each product (can use package for display)
 
   //figure out how to use table package to put in headers
   // head[("ID", "brand", "shoename", "colorway", "price", "stock")],
 
-  const data = products.map(product => [
-    product.id,
-    product.brand,
-    product.shoename,
-    product.colorway,
-    product.price,
-    product.stock
-  ]);
-  console.log(table(data));
-};
-start();
+// 
